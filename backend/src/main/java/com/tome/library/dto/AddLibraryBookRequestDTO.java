fb876a056b2182bc3acb2153ad369a3fadb0b580
Eduardo <elozano2a@gmail.com>
2026-08-19 12:02:37 -0500

    Run spotless apply

--- backend/src/main/java/com/tome/library/dto/AddLibraryBookRequestDTO.java
+++ backend/src/main/java/com/tome/library/dto/AddLibraryBookRequestDTO.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 public class AddLibraryBookRequestDTO {
   @NotNull private UUID bookId;
 
-  public AddLibraryBookRequestDTO() {} //NOSONAR - required by Jackson
+  public AddLibraryBookRequestDTO() {} // NOSONAR - required by Jackson
 
   public AddLibraryBookRequestDTO(UUID bookId) {
     this.bookId = bookId;
